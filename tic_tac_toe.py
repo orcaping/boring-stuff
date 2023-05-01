e14df25b4a3ac224fe2890c577af94f2d3402a6f
Yanick Egli <nick.eglirohr@gmail.com>
2023-05-01 11:38:46 +0200

    updating tictactoe

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,3 +3,23 @@
 theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+def print_board(board):
+    """Board Design."""
+    print(board['top-L'] + ' |' + board['top-M'] + ' |' + board['top-R'])
+    print('--+--+--')
+    print(board['mid-L'] + ' |' + board['mid-M'] + ' |' + board['mid-R'])
+    print('--+--+--')
+    print(board['low-L'] + ' |' + board['low-M'] + ' |' + board['low-R'])
+
+
+turn = 'X'
+for i in range(9):
+    print_board(theBoard)
+    move = input('Turn for ' + turn + ' on which space?: ')
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+print_board(theBoard)
