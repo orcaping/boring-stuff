4630d8b382ec112d2fcc6576336d5b1852e98d2e
Yanick Egli <nick.eglirohr@gmail.com>
2023-05-01 11:22:49 +0200

    adding tictactoe

---
+++ tic_tac_toe.py
@@ -0,0 +1,5 @@
+"""Game of TicTacToe."""
+
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
