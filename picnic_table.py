fa0b262ac8b36f0a9565748a3a14548fbb5f6869
Yanick Egli <nick.eglirohr@gmail.com>
2023-05-06 09:18:42 +0200

    fixing picnic_table.py

--- picnic_table.py
+++ picnic_table.py
@@ -1,8 +1,9 @@
 """clean human readable list output"""
 def print_picnic(items_dict, left_width, right_width):
+    """takes input and rearanges into well readable table"""
     print('PICNIP ITEMS'.center(left_width + right_width, '_'))
-    for k, v in items_dict.items():
-        print(k.ljust(left_width, '.') + str(v).rjust(right_width))
+    for k, value in items_dict.items():
+        print(k.ljust(left_width, '.') + str(value).rjust(right_width))
 
 picnic_items = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
 print_picnic(picnic_items, 12, 5)
